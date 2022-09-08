a00c979233ef7776b00e823ae536c21c6de79ca9
Than Tran Phuoc <59443327+phuocthan@users.noreply.github.com>
2022-09-08 10:16:34 +0700

    - update build

--- build/web-mobile/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","mainChar","UI","NPC"],collisionMatrix:[[true],[false,false,null,true],[false,false,false],[false,true,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"",jsList:[]};
